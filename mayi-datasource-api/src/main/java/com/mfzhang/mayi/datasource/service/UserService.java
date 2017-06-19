654b412385812970b169f3275372c20eb2495aa4
mingfei.zhang <409244379@qq.com>
2017-06-19 22:35:23 +0800

    环境搭建完善

---
+++ mayi-datasource-api/src/main/java/com/mfzhang/mayi/datasource/service/UserService.java
@@ -0,0 +1,12 @@
+package com.mfzhang.mayi.datasource.service;
+
+/**
+ * 
+ *
+ * @author Guo YL 2017年6月18日 下午6:48:22
+ */
+public interface UserService {
+
+	// getUserById(Integer id);
+	
+}
---
+++ mayi-datasource-service/src/main/java/com/mfzhang/mayi/datasource/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.mfzhang.mayi.datasource.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // 表明是配置类
+@ComponentScan("com.mfzhang.mayi.datasource") // 扫描包
+public class WebConfig {
+
+	
+	
+}
