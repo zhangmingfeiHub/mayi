34e804cc88c205fd2151f4127b90dc09e6700c21
张明飞 <mingfei.zhang@chinaredstar.com>
2017-12-22 15:33:43 +0800

    A

---
+++ mayi-test/src/main/java/com/mfzhang/mayi/test/basic/Test1.java
@@ -0,0 +1,10 @@
+package com.mfzhang.mayi.test.basic;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String str = "同步龙果线上合同与合同的线上合同结束时间一致（含合同状态一致）-合同有效但已是逻辑删除状态数据修复-一个店铺多个合同";
+		System.out.println("length: " + str.length());
+	}
+	
+}
