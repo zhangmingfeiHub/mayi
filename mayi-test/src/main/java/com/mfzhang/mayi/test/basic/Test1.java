c30dab1a471e741fe5ce42421afc77906f9c3d05
张明飞 <mingfei.zhang@chinaredstar.com>
2018-04-12 12:10:07 +0800

    a

--- mayi-test/src/main/java/com/mfzhang/mayi/test/basic/Test1.java
+++ mayi-test/src/main/java/com/mfzhang/mayi/test/basic/Test1.java
@@ -1,15 +1,16 @@
 package com.mfzhang.mayi.test.basic;
 
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
+
+import com.mfzhang.mayi.common.utils.CommonUtils;
 
 public class Test1 {
 
 	public static void main(String[] args) {
-		String str = "同步龙果线上合同与合同的线上合同结束时间一致（含合同状态一致）-合同有效但已是逻辑删除状态数据修复-一个店铺多个合同";
-		System.out.println("length: " + str.length());
-		
-		String dateStr = new Date().toString();
-		System.out.println("dateStr: " + dateStr);
+		List<String> list = Collections.singletonList("a");
+		System.err.println(CommonUtils.writeValueAsString(list));
 	}
 	
 }
